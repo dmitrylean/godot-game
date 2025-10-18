2ffdbe156859b84958a6c20236c50924b97d74da
dmitrylean <adam.lean.92@gmail.com>
2025-10-18 12:22:54 +0800

    update

--- player/player.cs
+++ player/player.cs
@@ -1,6 +1,19 @@
 using Godot;
 using System;
 
-public partial class player : Node
+public partial class Player : CharacterBody3D
 {
+  [Export] public float Speed = 5.0f;
+
+  public override void _PhysicsProcess(double delta)
+  {
+    Vector3 input = new(
+        Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+        0,
+        Input.GetActionStrength("move_back") - Input.GetActionStrength("move_forward")
+    );
+
+    Velocity = input.Normalized() * Speed;
+    MoveAndSlide();
+  }
 }
